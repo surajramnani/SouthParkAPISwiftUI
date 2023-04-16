5ca6accb58ad4f64df3951a7826d50581931a083
Suraj Ramnani <surajramnani@gmail.com>
2023-04-16 22:40:36 +0530

    Initial Commit

---
+++ SouthParkAPISwiftUI/SouthParkAPISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SouthParkAPISwiftUIApp.swift
+//  SouthParkAPISwiftUI
+//
+//  Created by Suraj Ramnani on 16/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SouthParkAPISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
