befb218eefb58770410fba79b6db38345d88bc09
Suraj Ramnani <surajramnani@gmail.com>
2023-04-17 01:18:58 +0530

    added Accent colour

--- SouthParkAPISwiftUI/ContentView.swift
+++ SouthParkAPISwiftUI/ContentView.swift
@@ -7,30 +7,47 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
     var body: some View {
         
-        TabView {
+        ZStack {
+         
+        
+            VStack {
+                
+                TabView {
 
-            SouthParkCharacters()
-                .tabItem{
-                    Label("Characters", systemImage: "figure.wave")
-                    Text("Characters")
+                    SouthParkCharacters()
+                        .tabItem{
+                            Label("Characters", systemImage: "figure.wave")
+                            Text("Characters")
+                            
+                        }
+                    
+                        
+                    
+                    SouthParkEpisodes()
+                        .tabItem{
+                            Label("Episodes", systemImage: "tv")
+                            Text("Episodes")
+                        }
+                    SouthParkOther()
+                        .tabItem{
+                            Label("Families", systemImage: "figure.2.and.child.holdinghands")
+                            Text("Families")
+                        }
+                        
                 }
-            SouthParkEpisodes()
-                .tabItem{
-                    Label("Episodes", systemImage: "tv")
-                    Text("Episodes")
-                }
-            SouthParkOther()
-                .tabItem{
-                    Label("Families", systemImage: "figure.2.and.child.holdinghands")
-                    Text("Families")
-                }
-            
+                
+                
+            }
+            .accentColor(.blue)
         }
-        .padding()
+
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
