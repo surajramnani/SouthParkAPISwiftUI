b88ad4393b5de03c11d88c1a9b6effaa2ea8ee34
Suraj Ramnani <surajramnani@gmail.com>
2023-04-17 09:31:13 +0530

    Grid View

--- SouthParkAPISwiftUI/SouthParkCharacters.swift
+++ SouthParkAPISwiftUI/SouthParkCharacters.swift
@@ -9,20 +9,32 @@ import SwiftUI
 
 struct SouthParkCharacters: View {
     var body: some View {
-       
-        List {
-            Link("Kyle", destination: URL(string: "https://hotmail.com")!)
-            Text("Cartman")
-            Text("Kenny")
-            Text("Butters")
+                
+                VStack {
+                    List {
+                        Link("Kyle", destination: URL(string: "https://hotmail.com")!)
+                        Text("Cartman")
+                        Text("Kenny")
+                        Text("Butters")
+                    }
+                    .listStyle(.sidebar)
+                    
+                    Grid(horizontalSpacing: 2, verticalSpacing: 2) {
+                        GridRow{
+                            Color.blue.frame(width: 160, height: 160)
+                            Color.blue.frame(width: 160, height: 160)
+                        }
+                    }
+                    .gridCellColumns(10)
+                }
+            }
         }
-        .listStyle(.sidebar)
-       
-    }
-}
 
-struct SouthParkCharacters_Previews: PreviewProvider {
-    static var previews: some View {
-        SouthParkCharacters()
+
+    struct SouthParkCharacters_Previews: PreviewProvider {
+        static var previews: some View {
+            SouthParkCharacters()
+        }
+        
     }
-}
+
