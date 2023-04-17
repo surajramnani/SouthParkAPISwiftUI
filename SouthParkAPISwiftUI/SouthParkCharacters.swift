9d68740a532bfc972a65e6fccf254441c0d521ab
Suraj Ramnani <surajramnani@gmail.com>
2023-04-17 10:18:18 +0530

    grid view setup

--- SouthParkAPISwiftUI/SouthParkCharacters.swift
+++ SouthParkAPISwiftUI/SouthParkCharacters.swift
@@ -7,28 +7,45 @@
 
 import SwiftUI
 
+
 struct SouthParkCharacters: View {
     var body: some View {
-                
-                VStack {
-                    List {
-                        Link("Kyle", destination: URL(string: "https://hotmail.com")!)
-                        Text("Cartman")
-                        Text("Kenny")
-                        Text("Butters")
+        
+        VStack {
+            List {
+                Link("Kyle", destination: URL(string: "https://hotmail.com")!)
+                Text("Cartman")
+                Text("Kenny")
+                Text("Butters")
+            }
+            .listStyle(.sidebar)
+            
+            HStack {
+                Grid(horizontalSpacing: 2, verticalSpacing: 2) {
+                    GridRow{
+                        Color.blue.frame(width: 180, height: 180)
+                        Color.blue.frame(width: 180, height: 180)
                     }
-                    .listStyle(.sidebar)
-                    
-                    Grid(horizontalSpacing: 2, verticalSpacing: 2) {
+                    GridRow{
+                        Color.blue.frame(width: 180, height: 180)
+                        Color.blue.frame(width: 180, height: 180)
+                    }
+                    GridRow {
                         GridRow{
-                            Color.blue.frame(width: 160, height: 160)
-                            Color.blue.frame(width: 160, height: 160)
+                            Color.blue.frame(width: 180, height: 180)
+                            Color.blue.frame(width: 180, height: 180)
                         }
+                        
+                        
                     }
-                    .gridCellColumns(10)
+                    
                 }
+                
+                
             }
         }
+    }
+}
 
 
     struct SouthParkCharacters_Previews: PreviewProvider {
