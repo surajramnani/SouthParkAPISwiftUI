2e1657dfa6d47a881a4d1aeb3add97aac104dd0a
Suraj Ramnani <surajramnani@gmail.com>
2023-04-19 11:21:41 +0530

    Almost complete

--- SouthParkAPISwiftUI/SouthParkLinks.swift
+++ SouthParkAPISwiftUI/SouthParkLinks.swift
@@ -10,44 +10,49 @@ import WebKit
 
 struct SouthParkLinks: View {
     var body: some View {
-            VStack {
+        VStack {
+            
+            List{
+                Section {
+                    NavigationLink("Official Site", destination:  WebView(url: URL(string: "https://www.southparkstudios.com/")!))
+                    NavigationLink("South Park Official Youtube Channel", destination:  WebView(url: URL(string: "https://www.youtube.com/channel/UC7R27sAWc_DqOldtI1JcYhQ")!))
+                    NavigationLink("South Park Official Merchandise", destination:  WebView(url: URL(string: "https://www.redwolf.in/south-park-merchandise-india")!))
+                    NavigationLink("South Park Discord", destination: WebView(url: URL(string:"https://discord.com/invite/rgW94f9f")!))
+                    
+                }
                 
-                List{
-                    Section {
-                        NavigationLink("Official Site", destination:  WebView(url: URL(string: "https://www.southparkstudios.com/")!))
-                        NavigationLink("South Park Official Youtube Channel", destination:  WebView(url: URL(string: "https://www.youtube.com/channel/UC7R27sAWc_DqOldtI1JcYhQ")!))
-                        NavigationLink("South Park Official Merchandise", destination:  WebView(url: URL(string: "https://www.redwolf.in/south-park-merchandise-india")!))
-                                                                                                             
-                    }
-                    
-                header: {
-                    Text("South Park Official")
-                    
-                }
-                footer: {
-                    Text("South Park Official channels")
-                    
-                }
-                    Section {
-                        NavigationLink("South Park funny moments", destination:  WebView(url: URL(string: "https://www.youtube.com/watch?v=lqvUuMpU554&ab_channel=LethalLev")!))
-                        NavigationLink("Best Eric Cartman Videos", destination:  WebView(url: URL(string: "https://www.youtube.com/watch?v=M6TrgH7SKks&pp=ygUKc291dGggcGFyaw%3D%3D")!))
-                        
-                    }
-                header: {
-                    Text("South Park Video Links")
-                    
-                }
-                footer: {
-                    Text("A few of my favorite videos from SouthPark")
-                    
-                }
-                    
-                }
-                .listStyle(.grouped)
+            header: {
+                Text("South Park Official")
+                
             }
+            footer: {
+                Text("South Park Official channels")
+                
+            }
+                Section {
+                    NavigationLink("South Park funny moments", destination:  WebView(url: URL(string: "https://www.youtube.com/watch?v=lqvUuMpU554&ab_channel=LethalLev")!))
+                    NavigationLink("Best Eric Cartman Videos", destination:  WebView(url: URL(string: "https://www.youtube.com/watch?v=M6TrgH7SKks&pp=ygUKc291dGggcGFyaw%3D%3D")!))
+                    
+                }
+            header: {
+                Text("South Park Video Links")
+                
+            }
+                Section {
+                    NavigationLink("South Park funny moments", destination:  WebView(url: URL(string: "https://www.youtube.com/watch?v=lqvUuMpU554&ab_channel=LethalLev")!))
+                    NavigationLink("Best Eric Cartman Videos", destination:  WebView(url: URL(string: "https://www.youtube.com/watch?v=M6TrgH7SKks&pp=ygUKc291dGggcGFyaw%3D%3D")!))
+                    
+                }
+            header: {
+                Text("My Top 10 Episodes")
+                
+            }
+            
+            }
+            .listStyle(.plain)
         }
         
-    }
+    }}
 
 struct WebView: UIViewRepresentable {
     let url: URL
--- SouthParkAPISwiftUI/SouthParkOther.swift
+++ SouthParkAPISwiftUI/SouthParkOther.swift
@@ -10,7 +10,16 @@ import SwiftUI
 struct SouthParkOther: View {
     @ObservedObject var post = PostManager()
     var body: some View {
-        
+        let array = ["McCorkmick Family" : "https://southpark.fandom.com/wiki/Category:McCormick_Family",
+                     "Adams-Malkowski Family" : "https://southpark.fandom.com/wiki/Mr._Adams",
+                     "Cartman Family" : "https://southpark.fandom.com/wiki/Category:Cartman/Tenorman_Family",
+                     "Kern / Kimble / Marsh Family" : "https://southpark.fandom.com/wiki/Category:Kern_/_Kimble_/_Marsh_Family",
+                     "Broflovski / Schwartz Family" : "https://southpark.fandom.com/wiki/Category:Broflovski_/_Schwartz_Family",
+                     "Anderson / O'Donnell Family" : "https://southpark.fandom.com/wiki/Millard_Anderson",
+                     "Barbrady Family" : "https://southpark.fandom.com/wiki/Officer_Barbrady",
+                     "Biggle Family" : "https://southpark.fandom.com/wiki/Mr._Biggle",
+                     "Black Family" : "https://southpark.fandom.com/wiki/Tolkien_Black",
+                     "Burch Family" : "https://southpark.fandom.com/wiki/Richard_Burch"] 
         ZStack
         {
             VStack {
@@ -21,9 +30,13 @@ struct SouthParkOther: View {
                     .clipShape(Circle()) // Apply a circular clip shape
                     .overlay(Circle().stroke(Color.blue, lineWidth: 8))
                 
-                List {
-                    ForEach(post.posts) { post in
-                        Text(post.name)
+                List(post.posts, id: \.id){ post in
+                        ForEach(array.keys.sorted(), id: \.self){ name in
+                            if name == post.name, let url = array[name] {
+                                NavigationLink(name, destination: DetailView(url:url))
+                            }
+                        }
+                    
                     }
                    
                 }
@@ -34,7 +47,7 @@ struct SouthParkOther: View {
             }
         }
     }
-}
+
 
 struct SouthParkOther_Previews: PreviewProvider {
     static var previews: some View {
