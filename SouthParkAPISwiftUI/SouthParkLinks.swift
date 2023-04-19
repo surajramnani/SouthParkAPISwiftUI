fe47adff1919a4bec685fa1820638693d5229856
Suraj Ramnani <surajramnani@gmail.com>
2023-04-19 13:25:29 +0530

    Complete

--- SouthParkAPISwiftUI/SouthParkLinks.swift
+++ SouthParkAPISwiftUI/SouthParkLinks.swift
@@ -39,12 +39,15 @@ struct SouthParkLinks: View {
                 
             }
                 Section {
-                    NavigationLink("South Park funny moments", destination:  WebView(url: URL(string: "https://www.youtube.com/watch?v=lqvUuMpU554&ab_channel=LethalLev")!))
-                    NavigationLink("Best Eric Cartman Videos", destination:  WebView(url: URL(string: "https://www.youtube.com/watch?v=M6TrgH7SKks&pp=ygUKc291dGggcGFyaw%3D%3D")!))
+                    NavigationLink("Scott Tenorman Must Die", destination:  WebView(url: URL(string: "https://www.southparkstudios.com/episodes/yf32xo/south-park-scott-tenorman-must-die-season-5-ep-4")!))
+                    NavigationLink("Cartmanland", destination:  WebView(url: URL(string: "https://www.southparkstudios.com/episodes/zm98ei/south-park-cartmanland-season-5-ep-6")!))
+                    NavigationLink("Wing", destination:  WebView(url: URL(string: "https://www.southparkstudios.com/episodes/h9tnne/south-park-wing-season-9-ep-3")!))
+                    NavigationLink("The Cissy", destination:  WebView(url: URL(string: "https://www.southparkstudios.com/episodes/2opbg6/south-park-the-cissy-season-18-ep-3")!))
+                    
                     
                 }
             header: {
-                Text("My Top 10 Episodes")
+                Text("My Favorite Episodes")
                 
             }
             
